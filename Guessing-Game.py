5b081cd74259c9d91e02ebdd3198c6c81c40a7af
JaeVeine Covington <jvncovington@gmail.com>
2022-05-06 17:53:03 -0400

    Update Guessing-Game.py
    updated game

--- Guessing-Game.py
+++ Guessing-Game.py
@@ -1,11 +1,40 @@
-import random
-i = 1
-number = int(random.randrange(0,10))
-guess = int(input("Guess What Number I'm Thinking Of Between 0-10? "))
 
-while guess != number:
-    print("Guess Again")
-    guess = int(input(""))
-    i += 1
+#New Guessing Game
 
-print("You Got It! It Took You " + str(i) + " Tries")
+comp_num = random.randint(0,10)
+attemps = 1
+
+# Main Game Loop
+def Game_Loop():
+    global attemps, player_guess
+    while attemps < 3 and player_guess != comp_num:
+        if player_guess != comp_num:
+            attemps+=1
+            print("Sorry, wrong number try again?\n")
+            player_guess = int(input("Guess A number from 0-10\n"))
+
+    if attemps >= 3:
+        print("Sorry you lost")
+    
+    if player_guess == comp_num:
+        print("You Win")
+
+    Replay()
+
+# Replay Function
+def Replay():
+    global attemps, player_guess, comp_num
+    replay_answer = input("Would You like to play again?: ").upper()
+    if replay_answer == "Y":
+        attemps = 1
+        comp_num = random.randint(0,10)
+        player_guess = int(input("Guess A number from 0-10\n"))
+        Game_Loop()
+    else:
+        print("Thanks For Playing")
+        quit()
+
+print("I'm thinking of a number can you guess what it is? I'll give you 3 tries to figure it out.")
+player_guess = int(input("Guess A number from 0-10\n"))
+
+Game_Loop()
